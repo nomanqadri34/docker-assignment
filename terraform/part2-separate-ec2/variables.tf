d4a538e62a432110d6a44e8f6799522c1cf60c18
nomanqadri34 <nomanqadri34@github.com>
2026-04-05 19:02:09 +0530

    Update AWS region to eu-west-1 for SSH Key

--- terraform/part2-separate-ec2/variables.tf
+++ terraform/part2-separate-ec2/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS Region"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-west-1"
 }
 
 variable "project_name" {
